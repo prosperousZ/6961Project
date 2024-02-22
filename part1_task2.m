4b175dffeabf3031ed83755c180f36cfc5241942
kclevr <159969972+kclevr@users.noreply.github.com>
2024-02-21 19:38:28 -0700

    Update part1_task2.m

--- part1_task2.m
+++ part1_task2.m
@@ -4,12 +4,12 @@
 clear all
 close all
 % Parameters
-M=1024;   %# of subcarriers
+M=2048;   %# of subcarriers
 N=4;    %Number of OFDM-QPSK SYMBOLS symbols
-L=5;    %taps
-%Delcare channel h
-h=[0.227, 0.46, 0.688, 0.46, 0.227];
-%h=[1 0 0 0 0];
+L=200;    %taps
+B=8e3;
+fc=24e3;
+
 %QPSK Symbol Map
 QPSKmap= [1+1j, -1+1j, -1-1j, 1-1j];
 s = zeros(M,1);
@@ -22,63 +22,108 @@ beta=0.125;
 delay=100;
 
 %Store 4 symbols here
-d_original = zeros(M,N);
+d_original = zeros(M*N,1);
 
 %result symbol
-d_result =  zeros(M,N);
+d_result =  zeros(M*N, 1);
+
+%Passband symbols
+xPB_syms= zeros(M*N,1);
+
+%Creates Chirp
+   t1=0.05;
+   t2=0.2;
+   f_0=fc-4000;
+   B_ch=B/t1;
+   t=[0:(t1*Fs)-1]/Fs;
+   f_t=f_0+B_ch*t;
+   ch=cos(2*pi*f_t.*t).';
+   ch_zp=zeros(t2*Fs, 1);
+   chirp=[ch; ch_zp; ch; ch_zp; ch; ch_zp; ch; ch_zp];
+ 
 
 %**************iteration start*********************
-for k=1:N
+for k=0:N-1
 %Generate 1 OFDM SYMBOL
   for j = 1 : M
      randomIndex = randi(length(QPSKmap), 1);
      s(j) = QPSKmap(randomIndex);%random choose from QPSK
-     d_original(j,k) = s(j);
+     d_original(j+k*M) = s(j);
   end
   
-    %Inverse FFT
+  %Inverse FFT
   x=ifft(s);
   
+  %Add zp
   tem = zeros(L-1,1);
-
   s_cp=[tem; x;tem];
-  %Y Output after going through channel impulse response of data w/o cp
 
   %upsample
   lambda=Fs/Fd;
   xup=upsample(s_cp, lambda);
+  t=[0:length(xup)-1]/Fs;
+  figure(), plot(t, abs(xup))
 
   %Square Root cosine Pulse
    sr_cos=rcosdesign(beta, 2*delay, lambda, 'sqrt');
-   %xBB=conv(xup, sr_cos);
-   figure(), impz(sr_cos)
-  
-   %Pass
- 
-
-  for i=L:M+2*(L-1)
-      y(i-(L-1))=h(1)*s_cp(i)+h(2)*s_cp(i-1)+h(3)*s_cp(i-2)+h(4)*s_cp(i-3)+h(5)*s_cp(i-4);
-  end
-  
- 
-  %step 5 from lecture 11 instructor' note which is similar to fft(y), but
-  %not the same
-  for ii = 1:M
-      sum = 0;
-      for kk = 1: M+(L-1)
-          sum = sum + y(kk) .* exp(-(1j*2*pi*(kk-1)*(ii-1))/M);
-      end
-      y_freq(ii) = sum;
-  end
-  
-  %step 6
-  h_freq = fft(h, M).';
-  d_freq=y_freq./h_freq;
-  for w = 1 : M
+   xBB=conv(xup, sr_cos);
+   
+   %RCos Plot
+   t=[0:length(sr_cos)-1]/Fs;         %Time Domain Plot
+   figure(), plot(t, abs(sr_cos))
     
-     d_result(w,k) = y_freq(w);
-  end
+   SR_cos=fftshift(fft(sr_cos));        %Freq Domain Plot
+   f=[-length(SR_cos)/2:(length(SR_cos))/2-1]*Fs;
+   figure(), plot(f, abs(SR_cos))
 
+   %Data Plot
+   t=[0:length(xBB)-1]/Fs;                       %Time Domain Plot
+   figure(), plot(t, abs(xBB))
+    
+   XBB=fftshift(fft(xBB));                     %Freq Domain Plot
+   f=[-length(xBB)/2:(length(xBB))/2-1]/length(xBB)*Fs;
+   figure(), plot(f, abs(XBB))
+  
+   %Passband
+   Ts=1/B;
+   ts=Ts/lambda;
+   n=[0:length(xBB)-1].';
+   xPB=real(exp(1j*2*pi*fc*n*ts).*xBB);
+   len_pb=length(xPB);
+   xPB_syms(1+k*len_pb:k*len_pb+len_pb)=xPB(1:len_pb);
+    
+   %Convert Signal back to baseband
+   xBB_i=xPB.*sin(2*pi*fc*n*ts)*2;
+   xBB_r=xPB.*cos(2*pi*fc*n*ts)*2;
+   xBB=xBB_r+1i*xBB_i;
+
+   %Match Filter
+   xOUF=conv(xBB, sr_cos);
+
+   %Down Sample
+   xBBd=xOUF(delay*lambda+1:lambda:end-delay*lambda);
+
+   %fft to convert symbols back into frequency domain
+    for ii = 1:M
+         sum = 0;
+         for kk = 1: M+(L-1)
+             sum = sum + xBBd(kk) .* exp(-(1j*2*pi*(kk-1)*(ii-1))/M);
+         end
+         y_freq(ii) = sum;
+    end
+   
+
+   d_result(1+k*M:k*M+M) = y_freq(1:M);
 end
+    
+   %Add Chirp to OFDM Symbol
+   xPB_ch=[chirp; xPB_syms; chirp];
+   
+   %Plot of chirp spectrogram
+   Nfft=4096;
+   figure(), spectrogram(xPB_ch, Nfft, Nfft*3/4, Nfft, Fs, 'yaxis' )
+   colormap jet
+   set(gca, 'Fontsize', 12, 'FontWeight', 'bold')
+   title('Transmission Spectrogram')
 %***********iteration end***************************
 %decide if bits are 1 or 0
